f4012a9b4a76f61e3a21443533c9709c66dbcac0
Robert Rossol <shyguyrbr1@gmail.com>
2017-03-20 12:49:52 -0500

    Make RESTful and Sortable

--- app/controllers/animals_controller.rb
+++ app/controllers/animals_controller.rb
@@ -1,9 +1,48 @@
 class AnimalsController < ApplicationController
 
 def index
+  sort=params[:sort] || "species"
+  direction=params[:order] || "asc"
+  @animals=Animal.all.order(sort => direction)
   render "index.html.erb"
 end
 
+def show
+  animal_id = params[:id]
+  @animal=Animal.find_by(id: animal_id)
+  render "show.html.erb"
+end
 
+def new
+  render "new.html.erb"
+end
+
+def create
+  animal = Animal.create(species: params[:species], number: params[:number])
+  animal_id=params[:id]
+  redirect_to "/animals"
+end
+
+def edit
+  animal_id = params[:id]
+  @animal=Animal.find_by(id: animal_id)
+  render "edit.html.erb"
+end
+
+def update
+  animal_id = params[:id]
+  @animal = Animal.find_by(id: animal_id)
+  @animal.species=params[:species]
+  @animal.number=params[:number]
+  @animal.save
+  redirect_to "/animals"
+end
+
+def destroy
+  animal_id=params[:id]
+  @animal=Animal.find_by(id: animal_id)
+  @animal.destroy
+  redirect_to "/animals"
+end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get "/animals" => "animals#index"
+  get "animals/new" =>"animals#new"
+  get "animals/:id" => "animals#show"
+  post "animals/" => "animals#create"
+  get "animals/:id/edit" => "animals#edit"
+  patch "animals/:id" => "animals#update"
+  delete "animals/:id" => "animals#destroy"
 end
